9e4c720cb0c7ab491f79b8991f05d6c512b5e130
anastasijaparhomenko <nastja100502@gmail.com>
2020-02-25 11:06:09 +0000

    scss

--- main.py
+++ main.py
@@ -30,7 +30,27 @@ def params_table():
   args = request.args
   return render_template('params_table.html', args = args)
 
+@app.route('/')
+def index_lapa():
+  return render_template('chats2.html')
 
+@app.route('/chats2/lasi')
+def ielasit_chatu():
+  chata_rindas = []
+  with open("chats2.txt", "r", encoding="UTF-8") as f:
+    for rinda in f:
+      chata_rindas.append(rinda)
+  return jsonify({"chats": chata_rindas})
+
+
+@app.route('/chats2/suuti', methods=['POST'])
+def suutiit_zinju():
+  dati = request.json
+  
+  with open("chats2.txt", "a", newline="", encoding="UTF-8") as f:
+    f.write(dati["chats"] + "\n")
+
+  return ielasit_chatu()
 
 @app.route('/chat')
 def chat():
